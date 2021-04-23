47264ff4b21bad95776e3fa2104d38d09e5a41d2
tomoko.morita <morita.techacademy@gmail.com>
2021-04-23 07:58:17 +0000

    kadaiteishutu1

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -14,17 +14,16 @@
                         {{-- 投稿内容 --}}
                         <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
                     </div>
-                    <div>
+                        <div style="display:inline-flex">
                         @if (Auth::id() == $micropost->user_id)
                             {{-- 投稿削除ボタンのフォーム --}}
                             {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
                                 {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
                             {!! Form::close() !!}
                         @endif
-                    </div>
-                @include('user_favorite.favorite_button')
-                </div>
 
+                        @include('user_favorite.favorite_button')
+                        </div>
             </li>
         @endforeach
     </ul>
--- resources/views/user_favorite/favorite_button.blade.php
+++ resources/views/user_favorite/favorite_button.blade.php
@@ -1,12 +1,12 @@
     @if (Auth::user()->is_favorites($micropost->id))
         {{--unnfavoriteボタンのフォーム --}}
         {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
-            {!! Form::submit('Unfavorite', ['class' => "btn btn-danger btn-block"]) !!}
+            {!! Form::submit('Unfavorite', ['class' => "btn btn-success btn-sm"]) !!}
         {!! Form::close() !!}
     @else
         {{-- favoriteボタンのフォーム --}}
         {!! Form::open(['route' => ['favorites.favorite', $micropost->id]]) !!}
-            {!! Form::submit('Favorite', ['class' => "btn btn-primary btn-block"]) !!}
+            {!! Form::submit('Favorite', ['class' => "btn btn-light btn-sm"]) !!}
         {!! Form::close() !!}
     @endif
 
